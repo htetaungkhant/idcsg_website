f0d8340dcd4ed738c6362684bb4bc0694ffd33e0
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-04 03:03:16 +0700

    feat: enhance Office Policies and Terms of Service pages with detailed content and improved styling

--- app/(main-pages)/(information-pages)/information/patient-info/terms-of-service/page.tsx
+++ app/(main-pages)/(information-pages)/information/patient-info/terms-of-service/page.tsx
@@ -2,10 +2,146 @@ import React from "react";
 
 export default function TermsOfService() {
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold mt-10">
-        Terms of Service Information
+    <div className="mx-auto w-[90%] pb-10">
+      <h1 className="text-6xl text-[#233259] font-semibold">
+        Terms of Service
       </h1>
+      <div className="mt-8 flex flex-col gap-y-4 text-lg text-[#233259] font-(family-name:--font-old-standard-tt)">
+        <p>Effective Date: June 16, 2024</p>
+        <div>
+          <p>Introduction</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+            nihil molestiae consequatur, vel illum.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Quis autem vel eum iure reprehenderit
+            qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
+            illum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+            nihil molestiae consequatur, vel illum.
+          </p>
+        </div>
+        <div>
+          <p>How We Use Your Information</p>
+          <p>We use your data for the following purposes:</p>
+          <ul className="list-disc pl-5">
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+              autem vel eum iure reprehenderit qui in ea voluptate velit esse
+              quam nihil molestiae consequatur, vel illum.Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Quis autem vel eum
+              iure reprehenderit qui in ea voluptate velit esse quam nihil
+              molestiae consequatur, vel illum.Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Quis autem vel eum iure
+              reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+              consequatur, vel illum.
+            </li>
+          </ul>
+          <p>Legal Basis for Data Processing</p>
+          <p>We process your data based on:</p>
+          <ul className="list-disc pl-5">
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+              autem vel eum iure reprehenderit qui in ea voluptate velit esse
+              quam nihil molestiae consequatur, vel illum.Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Quis autem vel eum
+              iure reprehenderit qui in ea voluptate velit esse quam nihil
+              molestiae consequatur, vel illum.Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Quis autem vel eum iure
+              reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+              consequatur, vel illum.
+            </li>
+          </ul>
+          <p>Data Protection Measures</p>
+          <p>To ensure the safety of your personal data, we implement:</p>
+          <ul className="list-disc pl-5">
+            <li>
+              ELorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+              autem vel eum iure reprehenderit qui in ea voluptate velit esse
+              quam nihil molestiae consequatur, vel illum.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+              autem vel eum iure reprehenderit qui in ea voluptate velit esse
+              quam nihil molestiae consequatur, vel illum.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Personal Data Breach Notification</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+            nihil molestiae consequatur, vel illum.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Quis autem vel eum iure reprehenderit
+            qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
+            illum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+            nihil molestiae consequatur, vel illum.
+          </p>
+        </div>
+        <div>
+          <p>Your Rights</p>
+          <p>You have the right to:</p>
+          <ul className="list-disc pl-5">
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+              autem vel eum iure reprehenderit qui in ea voluptate velit esse
+              quam nihil molestiae consequatur, vel illum.Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Quis autem vel eum
+              iure reprehenderit qui in ea voluptate velit esse quam nihil
+              molestiae consequatur, vel illum.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p>Data Retention</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+            nihil molestiae consequatur, vel illum.Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Quis autem vel eum iure reprehenderit
+            qui in ea voluptate velit esse quam nihil molestiae consequatur, vel
+            illum.
+          </p>
+        </div>
+        <div>
+          <p>Changes to This Policy</p>
+          <p>
+            We may update this policy from time to time. Any changes will be
+            reflected on this page, and continued use of our website constitutes
+            acceptance of these updates.
+          </p>
+        </div>
+        <div>
+          <p>Contact Us</p>
+          <p>
+            For privacy-related inquiries, contact us at: iLorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Quis autem vel eum iure
+            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+            consequatur, vel illum.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
