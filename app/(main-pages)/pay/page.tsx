34fb91f342678b67f96faa12210f8ca4cbbf749c
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-03 22:11:07 +0700

    feat: add custom Select component using Radix UI Select
    - Implemented a new Select component with various subcomponents (SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectLabel, SelectItem, SelectSeparator, SelectScrollUpButton, SelectScrollDownButton).
    - Styled the Select component with Tailwind CSS classes for consistent design.
    - Added @radix-ui/react-select dependency to package.json and updated package-lock.json.

--- app/(main-pages)/pay/page.tsx
+++ app/(main-pages)/pay/page.tsx
@@ -1,25 +1,14 @@
 import React from "react";
-import Image from "next/image";
+
+import PaymentForm from "./(components)/PaymentForm";
 
 export default function Pay() {
   return (
-    <div className="-mt-9 px-4 flex flex-col items-center gap-6">
-      <Image
-        src="/under_construction.svg"
-        alt="Pay Image"
-        width={300}
-        height={100}
-        className="w-72 h-auto object-contain"
-      />
-      <h1 className="text-center text-[#233259] text-5xl lg:text-7xl font-medium">
-        We are Coming Soon!!!
-      </h1>
-      <p className="text-center text-[#6C6C6C] text-lg lg:text-2xl font-(family-name:--font-ubuntu)">
-        Stay tuned for something amazing
-      </p>
-      <span className="text-center text-[#9C1016] text-xl lg:text-3xl font-(family-name:--font-ubuntu)">
-        80 Days : 10 Hrs : 46 Mins : 13 Secs
-      </span>
+    <div className="pb-10">
+      <div className="relative flex justify-center py-8 lg:py-16">
+        <div className="absolute w-[70%] min-w-[280px] inset-0 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-[#2E374E] to-[#314765]" />
+        <PaymentForm className="w-[85%]" />
+      </div>
     </div>
   );
 }
