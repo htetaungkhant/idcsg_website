ed4b48bd95efe1a874092f403ed955130c3c1f54
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-09 12:56:11 +0700

    feat: enhance Patient Forms page layout and content

--- app/(main-pages)/(information-pages)/information/patient-info/patient-forms/page.tsx
+++ app/(main-pages)/(information-pages)/information/patient-info/patient-forms/page.tsx
@@ -1,11 +1,35 @@
 import React from "react";
+import Image from "next/image";
+
+import { PrimaryBtn1 } from "@/components/CustomButtons";
 
 export default function PatientForms() {
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold mt-10">
-        Patient Forms Information
-      </h1>
+    <div className="flex flex-col gap-20 justify-center mx-auto w-[90%] pb-20">
+      <div className="relative">
+        <h1 className="absolute top-0 left-0 px-5 py-5 w-full h-40 bg-gradient-to-b from-[#000000] to-[#00000000] text-white text-5xl rounded-t-2xl">
+          Patient Forms
+        </h1>
+        <Image
+          src="/3.png"
+          width={500}
+          height={300}
+          alt="Patient Forms"
+          className="w-full h-auto object-cover rounded-2xl shadow-sm"
+        />
+      </div>
+      <div className="text-[#233259]">
+        <h2 className="font-(family-name:--font-ubuntu) font-bold text-2xl">
+          Patient Intake Information
+        </h2>
+        <p className="font-(family-name:--font-old-standard-tt) text-lg mt-4">
+          Please click this link to register your new patient intake information
+          and health history. Our office will text you this link when you make
+          an actual appointment with our office. This is a reference in case you
+          did not receive the text link.
+        </p>
+        <PrimaryBtn1 className="mx-auto mt-10">Click to Apply</PrimaryBtn1>
+      </div>
     </div>
   );
 }
