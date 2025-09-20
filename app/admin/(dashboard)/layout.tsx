51c83635af7e3745aaff5383f2c5398f9cb81bb7
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-20 11:06:22 +0700

    feat: update DashboardLayout to accept children as props for better component structure

--- app/admin/(dashboard)/layout.tsx
+++ app/admin/(dashboard)/layout.tsx
@@ -3,12 +3,17 @@ import { redirect } from "next/navigation";
 import { SignOut } from "@/components/SignOut";
 import { auth } from "@/lib/auth";
 
-export default async function DashboardLayout() {
+export default async function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   const session = await auth();
   if (!session) redirect("/admin/sign-in");
 
   return (
     <div className="flex flex-col min-h-screen">
+      {children}
       <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
         <h1 className="text-lg font-bold">Admin Panel</h1>
         <SignOut />
