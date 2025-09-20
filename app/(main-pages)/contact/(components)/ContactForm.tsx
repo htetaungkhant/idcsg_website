d1b3442d671a11ba10b278331eda21c484a2734c
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-20 10:25:13 +0700

    fix: add a newline for better readability in ContactForm component

--- app/(main-pages)/contact/(components)/ContactForm.tsx
+++ app/(main-pages)/contact/(components)/ContactForm.tsx
@@ -38,6 +38,7 @@ export default function ContactForm({ className }: ContactFormProps) {
     console.log(values);
     // Handle form submission here
   }
+
   return (
     <Form {...form}>
       <form
