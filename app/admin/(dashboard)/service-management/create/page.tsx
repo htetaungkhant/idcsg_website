d611f3682d01b8f4a4f1cf2a81dd1b7d1f829bd5
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-23 23:01:26 +0700

    feat: make CreateServicePage asynchronous for improved data handling

--- app/admin/(dashboard)/service-management/create/page.tsx
+++ app/admin/(dashboard)/service-management/create/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BackNavigation from "@/app/admin/(dashboard)/(components)/BackNavigation";
 import CreateServiceForm from "@/app/admin/(dashboard)/service-management/(components)/CreateServiceForm";
 
-export default function CreateServicePage() {
+export default async function CreateServicePage() {
   return (
     <div className="space-y-6">
       {/* Back Navigation */}
