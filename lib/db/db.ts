55cd90c74d1ac8505a8cdd2df16801fcd2a20bb1
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-09-26 17:19:45 +0700

    feat: Enhance PrismaClient initialization with transaction timeout configuration

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -6,7 +6,11 @@ const globalForPrisma = global as unknown as {
 };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ||
+  // 180000 means 3 minutes
+  new PrismaClient({ transactionOptions: { timeout: 180000 } }).$extends(
+    withAccelerate()
+  );
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
