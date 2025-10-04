d1b1c62ccfa62049a09891dee12d787e24d9b7ca
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-10-05 01:58:03 +0700

    feat: Implement PatientInstructions component with dynamic data rendering and custom card display; add SVG icons for enhanced visuals

--- app/(main-pages)/information/patient-info/patient-instructions/page.tsx
+++ app/(main-pages)/information/patient-info/patient-instructions/page.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import Image from "next/image";
+import { PatientInstructionsService } from "@/lib/services/patient-instructions-service";
+import { CustomCard2 } from "@/components/CustomCard";
 
-export default function PatientInstructions() {
+export default async function PatientInstructions() {
+  const patientInstructionsData =
+    await PatientInstructionsService.getPatientInstructions();
+
+  if (!patientInstructionsData) {
+    return (
+      <div className="mx-auto w-[90%]">No patient instructions available.</div>
+    );
+  }
   return (
-    <div>
-      <h1 className="text-center text-3xl font-bold mt-10">
-        Patient Instructions Information
-      </h1>
+    <div className="flex flex-col gap-20 justify-center mx-auto w-[90%] pb-20">
+      {patientInstructionsData.bannerImage && (
+        <div className="relative">
+          <h1 className="absolute top-0 left-0 px-5 py-5 w-full h-40 bg-gradient-to-b from-[#000000] to-[#00000000] text-white text-5xl rounded-t-2xl">
+            Patient Instructions
+          </h1>
+          <Image
+            src="/3.png"
+            width={500}
+            height={300}
+            alt="Patient Forms"
+            className="w-full h-auto object-cover rounded-2xl shadow-sm"
+          />
+        </div>
+      )}
+      {patientInstructionsData.cards &&
+        patientInstructionsData.cards.length > 0 && (
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-6">
+            {patientInstructionsData.cards.map((card) => (
+              <CustomCard2
+                key={card.id}
+                icon="/patient-instructions-icon.svg"
+                title={card.contentTitle}
+                contentImageUrl={card.contentImage}
+                description={card.contentDescription}
+                bgImageUrl={card.backgroundImage}
+              />
+            ))}
+          </div>
+        )}
     </div>
   );
 }
--- components/CustomCard.tsx
+++ components/CustomCard.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 interface CustomCard1Props {
   icon?: string | React.ReactNode;
@@ -13,13 +20,13 @@ interface CustomCard1Props {
   titleDataAttr?: string;
 }
 export const CustomCard1 = ({
-  className,
   icon,
   title,
   bgImageUrl,
   size = "sm",
   description,
   titleDataAttr,
+  className,
 }: CustomCard1Props) => {
   return (
     <div
@@ -70,5 +77,149 @@ export const CustomCard1 = ({
         />
       )}
     </div>
+  );
+};
+
+interface CustomCard2Props {
+  icon?: string;
+  title?: string | null;
+  description: string | null;
+  contentImageUrl?: string | null;
+  bgImageUrl?: string | null;
+  className?: string;
+}
+export const CustomCard2 = ({
+  icon,
+  title,
+  description,
+  contentImageUrl,
+  bgImageUrl,
+  className,
+}: CustomCard2Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div
+          className={cn(
+            "cursor-pointer bg-cover bg-center bg-no-repeat w-full h-80 lg:h-100 flex flex-col justify-center items-center gap-y-6 text-white font-(family-name:--font-ubuntu) rounded-2xl overflow-hidden relative",
+            className
+          )}
+          style={{
+            backgroundImage: bgImageUrl
+              ? `url(${bgImageUrl})`
+              : "url('/4.png')",
+          }}
+        >
+          <div className="absolute inset-0 bg-[#000000B8]" />
+          {icon && (
+            <Image
+              src={icon}
+              width={100}
+              height={100}
+              alt={title || "icon"}
+              className="w-24 lg:w-32 h-auto object-contain relative"
+            />
+          )}
+          {title && (
+            <h2 className="font-bold text-2xl lg:text-3xl xl:text-4xl relative px-10 lg:px-20 text-center line-clamp-3">
+              {title}
+            </h2>
+          )}
+        </div>
+      </DialogTrigger>
+      <DialogContent
+        showCloseButton={false}
+        className="z-100 gap-0 p-0 sm:max-w-[90vw] w-[90%] lg:max-w-400 text-[#233259] bg-radial from-[#FFFFFF] to-[#D2F7FF] outline-none focus-visible:ring-0"
+      >
+        <DialogTitle />
+        <DialogClose asChild>
+          <Image
+            src="/cross-icon.png"
+            alt="Close"
+            width={24}
+            height={24}
+            className="absolute top-2 right-2 cursor-pointer z-10"
+          />
+        </DialogClose>
+        <div className="px-4 py-4 lg:px-6 lg:py-6 max-h-[90vh] overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+          {title && (
+            <h1 className="text-3xl lg:text-4xl xl:text-6xl font-bold lg:w-[50%] mx-auto text-center font-(family-name:--font-oswald)">
+              {title}
+            </h1>
+          )}
+          <div className="mt-4 lg:mt-12">
+            {contentImageUrl && (
+              <div className="pl-5 pb-2 float-right">
+                <Image
+                  src={contentImageUrl}
+                  alt="content image"
+                  width={500}
+                  height={300}
+                  className="w-80 h-80 object-cover rounded-lg"
+                />
+              </div>
+            )}
+            {description && (
+              <p
+                className="whitespace-pre-wrap text-sm lg:text-base font-(family-name:--font-ubuntu)"
+                dangerouslySetInnerHTML={{ __html: description }}
+              />
+            )}
+          </div>
+
+          {/* <div className="">
+            <Image
+              src={contentImageUrl!}
+              alt="Temporary Crown Care"
+              width={500}
+              height={300}
+              className="rounded-lg float-right"
+            />
+            <p className="">
+              The process to prepare your tooth to receive a permanent crown is
+              very irritating both to the prepared tooth and its surrounding gum
+              tissue. You may experience tenderness or a dull, achy feeling in
+              this area for the next few days to one week. You may take any Over
+              the Counter (OTC) analgesic for the discomfort. While your
+              permanent crown is being fabricated, you will wear the temporary
+              acrylic crown we have fabricated and placed with temporary cement.
+              Because it is a temporary crown, you will care for it differently
+              than you would your natural teeth. The material we use to make
+              your temporary crown is a dental acrylic. It is not as strong a
+              your permanent one will be. The acrylic will tend to expand, flex,
+              bend and wear more easily than a permanent crown. Therefore, we
+              strongly suggest watching your diet, doing the majority of your
+              chewing away from your temporary crown. Do not subject your
+              temporary crown to undue stress in function such as biting into
+              Jolly Ranchers, ice, gum or hard nuts. Rule of thumb: nothing
+              sticky, chewy, or crunchy while in your temporary. If you wear any
+              type of appliance, (example: TMJ orthotic, sleep appliances,
+              retainers) you cannot wear it during temporary stage or it will
+              pull off your temporary crown.
+              The process to prepare your tooth to receive a permanent crown is
+              very irritating both to the prepared tooth and its surrounding gum
+              tissue. You may experience tenderness or a dull, achy feeling in
+              this area for the next few days to one week. You may take any Over
+              the Counter (OTC) analgesic for the discomfort. While your
+              permanent crown is being fabricated, you will wear the temporary
+              acrylic crown we have fabricated and placed with temporary cement.
+              Because it is a temporary crown, you will care for it differently
+              than you would your natural teeth. The material we use to make
+              your temporary crown is a dental acrylic. It is not as strong a
+              your permanent one will be. The acrylic will tend to expand, flex,
+              bend and wear more easily than a permanent crown. Therefore, we
+              strongly suggest watching your diet, doing the majority of your
+              chewing away from your temporary crown. Do not subject your
+              temporary crown to undue stress in function such as biting into
+              Jolly Ranchers, ice, gum or hard nuts. Rule of thumb: nothing
+              sticky, chewy, or crunchy while in your temporary. If you wear any
+              type of appliance, (example: TMJ orthotic, sleep appliances,
+              retainers) you cannot wear it during temporary stage or it will
+              pull off your temporary crown.
+            </p>
+          </div> */}
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
