b220e8a635da756b5c0dfa1912f29f06eaa541f6
Htet Aung Khant <chitkogyi19950@gmail.com>
2025-10-05 00:37:09 +0700

    fix: Comment out unused Image imports in Blog and Shop components

--- app/(main-pages)/shop/page.tsx
+++ app/(main-pages)/shop/page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Shop() {
   return (
     <div className="-mt-9 px-4 flex flex-col items-center gap-6">
-      <Image
+      {/* <Image
         src="/under_construction.svg"
         alt="Shop Image"
         width={300}
         height={100}
         className="w-72 h-auto object-contain"
-      />
-      <h1 className="text-center text-[#233259] text-5xl lg:text-7xl font-medium">
+      /> */}
+      <h1 className="pt-32 text-center text-[#233259] text-5xl lg:text-7xl font-medium">
         We are Coming Soon!!!
       </h1>
       <p className="text-center text-[#6C6C6C] text-lg lg:text-2xl font-(family-name:--font-ubuntu)">
